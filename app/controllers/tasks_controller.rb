705eca364f8e7f052f26686cf33970e767cbd8a1
Michael Belzil <michaelbelzil@gmail.com>
2018-08-07 14:32:07 -0400

    all working, not refactored though

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,35 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new # This is just for `form_for`
+  end
+
+  def create
+    @task = Task.new(title: params[:task][:title], details: params[:task][:details])
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(title: params[:task][:title], details: params[:task][:details], completed: params[:task][:completed])
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def task_params
+  end
 end
